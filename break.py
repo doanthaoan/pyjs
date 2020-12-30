6ec781db5f4e9a68f3f1c1d5b7e2e4f722ac8283
andt <andt@neotiq.net>
2020-12-30 16:12:51 +0700

    add 2 methods and compare

--- break.py
+++ break.py
@@ -1,8 +1,24 @@
+print("Using a check variable: ")
+a1 = []
 for n in range(2, 10):
+    check = True
     for x in range(2, n):
         if n % x == 0:
-            print(n, '=', x , '*', n//x)
-            continue
-        else:
-            print(n)
-            continue
+            check = False
+    if check:
+        a1.append(n)
+        print(n)
+print("----------")        
+print("Using break and ELSE for loop: ")
+a2 = []
+for n in range(2,10):
+    for x in range(2, n):
+        if n % x == 0:
+            break
+    else:
+        a2.append(n)
+        print(n)
+
+print("----------")
+print("Compare 2 result:", a1 == a2)
+print(a1)
